cdc67371bc2849ee89c183fd706965f89b3c9b19
mohammed dhedy <mhmddhedy1234@gmail.com>
2026-01-22 20:47:51 +0200

    fix cors for netlify

--- Program.cs
+++ Program.cs
@@ -12,23 +12,27 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "https://campuscircleaaup.netlify.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseCors("AllowFrontend");
-app.MapControllers();
 app.UseStaticFiles();
+
+app.UseCors("AllowFrontend");
+
+app.MapControllers();
 app.Run();
--- obj/Debug/net8.0/CampusCircle.AssemblyInfo.cs
+++ obj/Debug/net8.0/CampusCircle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusCircle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bf1dcf5e4a9649dbe5009c58c50bcd3b6963f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusCircle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusCircle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
